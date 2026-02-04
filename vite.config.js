8873eb9df6d088f18513a227e7ee3127f2222031
saminder-chahal <chahalsaminder13@gmail.com>
2026-02-04 17:35:10 -0500

    Change base URL for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/saminder-chahal.github.io/',
   build: {
     outDir: 'dist'
   }
